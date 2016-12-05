f0ff7dd853988f99d4f318173c1ce01bf464b451
Michael Yabut <spanishiwa@gmail.com>
2016-12-05 15:04:01 -0800

    snakeBite logic fixed. reverting game speed back to 80 ms

--- src/bundle.js
+++ src/bundle.js
@@ -84,7 +84,7 @@
 	
 	    document.addEventListener("keydown", this.handleKeyEvent.bind(this));
 	
-	    this.gamePlaying = window.setInterval(this.step.bind(this), 300);
+	    this.gamePlaying = window.setInterval(this.step.bind(this), 80);
 	  }
 	
 	  _createClass(View, [{
